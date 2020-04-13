37c281798a9c8b89c86b78908ac622fe009ddc39
ks2013945042 <48702252+ks2013945042@users.noreply.github.com>
2020-04-13 17:37:34 +0900

    init수정

---
+++ src/test/java/com/taegyu/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taegyu.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
